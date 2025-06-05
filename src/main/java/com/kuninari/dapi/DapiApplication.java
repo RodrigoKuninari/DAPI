467dfdc2dd8726177afcc14f9a9e485a26f268f7
RodrigoKuninari <rodrigokuninari@outlook.com>
2025-06-05 07:59:51 -0300

    Initial Setup

---
+++ src/main/java/com/kuninari/dapi/DapiApplication.java
@@ -0,0 +1,13 @@
+package com.kuninari.dapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kuninari/dapi/DapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuninari.dapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
